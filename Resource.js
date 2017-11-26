012657c2f8902dd3d6cf767ec46ccf24173c6086
bitagi97 <bitagi1997@gmail.com>
2017-11-26 13:27:22 +0700

    resource

---
+++ Resource.js
@@ -0,0 +1,116 @@
+/**
+ * Created by GSN on 6/2/2015.
+ */
+
+
+var res = {
+    //font
+    FONT_BITMAP_NUMBER_1:"fonts/number_1.fnt",
+    FONT_BITMAP_DICE_NUMBER: "fonts/diceNumber.fnt",
+    //zcsd
+    //screen
+    ZCSD_SCREEN_MENU:"zcsd/screen_menu.json",
+    ZCSD_SCREEN_NETWORK:"zcsd/screen_network.json",
+    ZCSD_SCREEN_LOCALIZATION:"zcsd/screen_localize.json",
+    ZCSD_SCREEN_DRAGON_BONES:"zcsd/screen_dragon_bones.json",
+    ZCSD_SCREEN_DECRYPTION:"zcsd/screen_decryption.json",
+    ZCSD_SCREEN_ZALO:"zcsd/screen_zalo.json",
+    //popup
+    ZCSD_POPUP_MINI_GAME:"zcsd/game/mini_game/PopupMiniGame.json",
+
+    //images
+    Slot1_png : "zcsd/slot1.png"
+};
+
+var BG_UNIT = "Art/Map/grass.png";
+var MOUNTAIN = "Art/Map/nui.png";
+var MO1 = "Art/Map/mo01.png";
+var TREE_ON_CORNER = "Art/Map/nhom-cay1 x 2.png";
+var SILO_IMG = "Art/Storage/silo/silo2-Levels 1.png";
+var LAYER_SILO = "Art/Storage/silo/silo2-Layer 188 copy 3.png";
+var mainShopImage = "content/Art/Shop/BG.png";
+var button1Image = "content/Art/Shop/button1.png";
+var button1_1Image = "content/Art/Shop/button1-1.png";
+var button2Image = "content/Art/Shop/button2.png";
+var button2_1Image = "content/Art/Shop/button2-1.png";
+var button3Image = "content/Art/Shop/button3.png";
+var button3_1Image = "content/Art/Shop/button3-1.png";
+var button4Image = "content/Art/Shop/button4.png";
+var button4_1Image = "content/Art/Shop/button4-1.png";
+
+var shopItemImage = "content/Art/Shop/SLOT.png";
+var shopIconMoney = "content/Art/Shop/gold1.png"
+var buttonShop = "content/Art/Main Gui/button-shop2.png";
+
+var animalTabConfig = "configs/shopTab/animalTab.json";
+var habitatTabConfig = "configs/shopTab/habitatTab.json";
+var treeTabConfig = "configs/shopTab/treeTab.json";
+
+
+var font20Normal ="content/Art/fonts/normal/20.fnt";
+var font20Outline = "content/Art/fonts/outline/20.fnt";
+var chickenImage = "content/Art/Shop/icon_vatnuoi/chicken.png";
+var shopButtonImage = "content/Art/Main Gui/button-shop2.png";
+var GRASS = "Art/Map/grass-lock2x2.png";
+var TRAIN = "Art/Map/ray tau.png";
+var MO2 = "Art/Map/mo02.png";
+var ROAD = "Art/Map/road.png";
+var RIVER1 = "Art/Map/song1.png";
+var RIVER2 = "Art/Map/song2.png";
+var RIVER3 = "Art/Map/song3.png";
+var TREE_ON_CORNER1 = "Art/Map/nhom-cay2 x 2.png";
+
+
+var g_resources = [
+    "CloseNormal.png",
+    "CloseSelected.png",
+    "game/animation/character/chipu/skeleton.xml",
+    "game/animation/eff_dice_number/skeleton.xml",
+    "game/animation/effDiceNumber/skeleton.xml",
+    "game/animation/firework_test/skeleton.xml",
+    "game/animation/ruongngusac/skeleton.xml",
+    "game/animation/Dragon/skeleton.json",
+    "game/animation/DragonBoy/skeleton.json",
+    "game/animation/lobby_girl/skeleton.xml",
+    "config.json",
+    "Default/Button_Disable.png",
+    "Default/Button_Normal.png",
+    "Default/Button_Press.png",
+    "favicon.ico",
+    "HelloWorld.png",
+    "fonts/diceNumber.fnt",
+    "fonts/diceNumber.png",
+    "fonts/eff_number.fnt",
+    "fonts/eff_number.png",
+    "fonts/number_1.fnt",
+    "fonts/number_1.png",
+    "game/animation/character/chipu/texture.plist",
+    "game/animation/character/chipu/texture.png",
+    "game/animation/eff_dice_number/texture.plist",
+    "game/animation/eff_dice_number/texture.png",
+    "game/animation/effDiceNumber/texture.plist",
+    "game/animation/effDiceNumber/texture.png",
+    "game/animation/firework_test/texture.plist",
+    "game/animation/firework_test/texture.png",
+    "game/animation/ruongngusac/texture.xml",
+    "game/animation/ruongngusac/texture.png",
+    "game/animation/Dragon/texture.json",
+    "game/animation/Dragon/texture.png",
+    "game/animation/DragonBoy/texture.json",
+    "game/animation/DragonBoy/texture.png",
+    "game/animation/lobby_girl/texture.plist",
+    "game/animation/lobby_girl/texture.png",
+    "ipConfig.json",
+    "localize/config.json",
+    "localize/vi.txt",
+    "localize/en.txt",
+    "shaders/change_color.fsh",
+    "zcsd/screen_decryption.json",
+    "zcsd/screen_dragon_bones.json",
+    "zcsd/screen_localize.json",
+    "zcsd/screen_menu.json",
+    "zcsd/screen_network.json",
+    "zcsd/screen_zalo.json",
+    "configs/mapInit.json"
+];
+
